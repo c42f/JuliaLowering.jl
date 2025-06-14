7a3df18df8b9be013d85e73c51905998de0ff07f
Shuhei Kadowaki <aviatesk@gmail.com>
2025-06-15 03:52:02 +0900

    precompile the whole lowering pipeline really

--- src/precompile.jl
+++ src/precompile.jl
@@ -3,7 +3,7 @@ if Base.get_bool_env("JULIA_LOWERING_PRECOMPILE", true)
     thunks = String[
         """
         function foo(xxx, yyy)
-            @nospecialize xxx yyy
+            @nospecialize xxx
             return Pair{Any,Any}(typeof(xxx), typeof(yyy))
         end
         """
@@ -20,7 +20,7 @@ if Base.get_bool_env("JULIA_LOWERING_PRECOMPILE", true)
         stream = JuliaSyntax.ParseStream(thunk)
         JuliaSyntax.parse!(stream; rule=:all)
         st0 = JuliaSyntax.build_tree(SyntaxTree, stream; filename=@__FILE__)
-        lwrst = lower(@__MODULE__, st0)
+        lwrst = lower(@__MODULE__, st0[1])
         lwr = to_lowered_expr(@__MODULE__, lwrst)
         @assert Meta.isexpr(lwr, :thunk) && only(lwr.args) isa Core.CodeInfo
     end
