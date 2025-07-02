456bbbd2e3d66b0234a12c163508e6b56199c48a
Shuhei Kadowaki <aviatesk@gmail.com>
2025-07-02 16:34:46 +0900

    add `include_location::Bool` option for `showprov`

--- src/utils.jl
+++ src/utils.jl
@@ -39,23 +39,26 @@ function _show_provtree(io::IO, prov, indent)
     printstyled(io, "@ $fn:$line\n", color=:light_black)
 end
 
-function showprov(io::IO, exs::AbstractVector; note=nothing, highlight_kwargs...)
+function showprov(io::IO, exs::AbstractVector;
+                  note=nothing, include_location::Bool=true, highlight_kwargs...)
     for (i,ex) in enumerate(Iterators.reverse(exs))
         sr = sourceref(ex)
         if i > 1
             print(io, "\n\n")
         end
         k = kind(ex)
-        if isnothing(note)
+        if isnothing(note) # use provided `note` otherwise
             note = i > 1 && k == K"macrocall"  ? "in macro expansion" :
                    i > 1 && k == K"$"          ? "interpolated here"  :
                    "in source"
         end
         highlight(io, sr; note=note, highlight_kwargs...)
 
-        line, _ = source_location(sr)
-        locstr = "$(filename(sr)):$line"
-        JuliaSyntax._printstyled(io, "\n# @ $locstr", fgcolor=:light_black)
+        if include_location
+            line, _ = source_location(sr)
+            locstr = "$(filename(sr)):$line"
+            JuliaSyntax._printstyled(io, "\n# @ $locstr", fgcolor=:light_black)
+        end
     end
 end
 
